1c096f009d12c80540944e3f9dc57625a474ecf2
Soner Koc <sonerkoc92@gmail.com>
2021-06-06 17:57:37 +0300

    fix

--- pipeline_train.py
+++ pipeline_train.py
@@ -51,7 +51,6 @@ def get_data(configurations):
     IMG_HEIGHT = configurations.size_img
     IMG_CHANNELS = 3
     TRAIN_PATH = configurations.data_folder
-    IMG_CHANNELS = 3
     
     # Path of Image Tiles and Masks
     path = TRAIN_PATH + "img/"
@@ -81,7 +80,6 @@ def get_data(configurations):
         if filter_tbud_count(path_bud_info, f, configurations.thold_tbud) == 0:
             continue
         img = cv2.imread(path + f)
-        print(path+f)
         img = cv2.resize(img, (IMG_HEIGHT, IMG_WIDTH), interpolation=cv2.INTER_AREA)
         img = img / 255
         X_train[i] = img
